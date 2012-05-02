df28b5379e279a8e131993b4fb3342d840acca5f
Petric Coroli <pcoroli@tacitknowledge.com>
2012-05-02 10:53:02 +0300

    XSD generation for XmlPropertyReader

---
+++ core/src/main/java/com/tacitknowledge/flip/model/package-info.java
@@ -0,0 +1,10 @@
+@XmlSchema (
+                xmlns = {},
+                namespace = "http://www.tacitknowledge.com/flip",
+                elementFormDefault = XmlNsForm.QUALIFIED,
+                attributeFormDefault = XmlNsForm.UNQUALIFIED
+                )
+package com.tacitknowledge.flip.model;
+
+import javax.xml.bind.annotation.XmlSchema;
+import javax.xml.bind.annotation.XmlNsForm;
