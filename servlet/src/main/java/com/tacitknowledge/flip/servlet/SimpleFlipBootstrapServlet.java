01adae422ba3ed59c47f29202990421e6ff99d8f
Scott Askew <scott@tacitknowledge.com>
2012-05-18 11:42:32 +0300

    Added a base servlet used to bootstrap Flip in a standward servlet environment.

---
+++ servlet/src/main/java/com/tacitknowledge/flip/servlet/SimpleFlipBootstrapServlet.java
@@ -0,0 +1,79 @@
+package com.tacitknowledge.flip.servlet;
+
+import com.tacitknowledge.flip.FeatureService;
+import com.tacitknowledge.flip.FeatureServiceReflectionFactory;
+import com.tacitknowledge.flip.FlipContext;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServlet;
+import java.io.IOException;
+
+/**
+ * A servlet used to bootstrap Flip in a standard web environment. This servlet should be used as a
+ * startup servlet. It looks for a "package-path" init property that should contain a comma-separated
+ * list of packages containing context and property providers.
+ *
+ * @author Scott Askew <scott@tacitknowledge.com>
+ */
+public class SimpleFlipBootstrapServlet extends HttpServlet
+{
+    /** The init param containing a comma-separated list of packages containing context and property providers */
+    public static final String PACKAGE_PATH = "package-path";
+
+    /** Package that are automatically included in the package path */
+    public static final String AUTOMATIC_PACKAGES = "com.tacitknowledge.flip.context, " +
+            "com.tacitknowledge.flip.servlet.properties";
+
+    /** The servlet config. */
+    private ServletConfig config;
+
+    @Override
+    public void init(ServletConfig config) throws ServletException
+    {
+        this.config = config;
+        String packagePath = config.getInitParameter(PACKAGE_PATH);
+        FeatureServiceReflectionFactory factory = new FeatureServiceReflectionFactory();
+        FeatureService service = factory.createFeatureService(getPackagesToSearch(packagePath));
+        FlipContext.setFeatureService(service);
+    }
+
+    @Override
+    public ServletConfig getServletConfig() {
+        return config;
+    }
+
+    @Override
+    public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException
+    {
+    }
+
+    @Override
+    public String getServletInfo() {
+        return "Simple Flip Bootstrap Servlet";
+    }
+
+    @Override
+    public void destroy() {
+    }
+
+    /**
+     * Returns a String array containing the list of packages to search for providers based on the servlet
+     * config. The core Flip providers will automatically be prepended to this list.
+     *
+     * @return a array of package names
+     */
+    protected String[] getPackagesToSearch(String packagePath)
+    {
+        if (packagePath == null)
+        {
+            packagePath = "";
+        }
+        packagePath = AUTOMATIC_PACKAGES + "," + packagePath;
+
+        // Split on either commas or colons and ignore leading and trailing whitespace
+        return packagePath.split("\\s*[,:;]\\s*");
+    }
+}
---
+++ servlet/src/test/java/com/tacitknowledge/flip/servlet/SimpleFlipBootstrapServletTest.java
@@ -0,0 +1,51 @@
+package com.tacitknowledge.flip.servlet;
+
+import static junit.framework.Assert.*;
+import static org.mockito.Mockito.*;
+
+import com.google.common.collect.ObjectArrays;
+import com.tacitknowledge.flip.FlipContext;
+import org.junit.Test;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import java.util.Enumeration;
+
+/**
+ * Tests for the SimpleFlipBootstrapServlet.
+ *
+ * @author Scott Askew <scott@tacitknowledge.com>
+ */
+public class SimpleFlipBootstrapServletTest
+{
+    @Test
+    public void testPackagePathSplitting()
+    {
+        SimpleFlipBootstrapServlet servlet = new SimpleFlipBootstrapServlet();
+        String[] expectedPath = buildExpectedPackagePath("foo", "bar", "baz", "bat");
+        // Test comma, colon, and semi-colon separators, with leading and trailing whitespace
+        String[] actualPath = servlet.getPackagesToSearch("foo, bar :baz;bat");
+
+        assertEquals(expectedPath.length, actualPath.length);
+
+        for (int i = 0; i < expectedPath.length; i++)
+        {
+            assertEquals(expectedPath[i], actualPath[i]);
+        }
+    }
+
+    @Test
+    public void testFeatureServiceIsSet() throws ServletException
+    {
+        SimpleFlipBootstrapServlet servlet = new SimpleFlipBootstrapServlet();
+        servlet.init(mock(ServletConfig.class));
+        assertNotNull(FlipContext.getFeatureService());
+    }
+
+    private String[] buildExpectedPackagePath(String... packages)
+    {
+        String[] autoPackages = SimpleFlipBootstrapServlet.AUTOMATIC_PACKAGES.split("\\s*[,:]\\s*");
+        return ObjectArrays.concat(autoPackages, packages, String.class);
+    }
+}
